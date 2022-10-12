cf4da5e7fc6f2894fd1e071f8d06ec63c0d67906
mujiburrohman <muji.official06@gmail.com>
2022-10-12 15:21:15 +0700

    validation login register

--- src/component/auth/Login.js
+++ src/component/auth/Login.js
@@ -1,40 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Formik, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 const Login = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const validationSchema = yup.object({
+    email: yup.string().required().email(),
+    password: yup.string().required().min(6),
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <>
       <h1>Login</h1>
-      <form>
-        <div className="col">
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              className="input"
-              name="email"
-              type="text"
-              placeholder="Email"
-            />
-            <span className="error-message">Error</span>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              className="input"
-              name="password"
-              type="text"
-              placeholder="Password"
-            />
-            <span className="error-message">Error</span>
-          </div>
-        </div>
-        <button className="btn btn-add">Login</button>
-        <span className="text-sm">
-          Belum punya akun? <Link to={'register'}>Register</Link>
-        </span>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(props) => {
+          return (
+            <Form>
+              <div className="col">
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <Field
+                    id="email"
+                    className="input"
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                  />
+                  <ErrorMessage name="email">
+                    {(err) => <span className="error-message">{err}</span>}
+                  </ErrorMessage>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <Field
+                    id="password"
+                    className="input"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                  />
+                  <ErrorMessage name="password">
+                    {(err) => <span className="error-message">{err}</span>}
+                  </ErrorMessage>
+                </div>
+              </div>
+              <button
+                className="btn btn-add"
+                disabled={!props.isValid || props.isSubmitting}
+              >
+                {props.isSubmitting ? 'Please Wait' : 'Login'}
+              </button>
+              <span className="text-sm">
+                Belum punya akun? <Link to={'/register'}>Register</Link>
+              </span>
+            </Form>
+          );
+        }}
+      </Formik>
     </>
   );
 };
--- src/component/auth/Register.js
+++ src/component/auth/Register.js
@@ -1,51 +1,91 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Formik, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 const Register = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  const validationSchema = yup.object({
+    email: yup.string().required().email(),
+    password: yup.string().required().min(6),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password'), null], 'Password must match'),
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
   return (
     <>
       <h1>Register</h1>
-      <form autoComplete="off">
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            className="input"
-            name="email"
-            type="text"
-            placeholder="Email"
-          />
-          <span className="error-message">Error</span>
-        </div>
-        <div className="col">
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              className="input"
-              name="password"
-              type="text"
-              placeholder="Password"
-            />
-            <span className="error-message">Error</span>
-          </div>
-          <div className="form-group">
-            <label htmlFor="confirm-password">Confirm Password</label>
-            <input
-              id="confirm-password"
-              className="input"
-              name="confirm-password"
-              type="password"
-              placeholder="Confirm Password"
-            />
-            <span className="error-message">Error</span>
-          </div>
-        </div>
-        <button className="btn btn-add">Register</button>
-        <span className="text-sm">
-          Sudah punya akun? <Link to={'register'}>Login</Link>
-        </span>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(props) => {
+          return (
+            <Form autoComplete="off">
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <Field
+                  id="email"
+                  className="input"
+                  name="email"
+                  type="text"
+                  placeholder="Email"
+                />
+                <ErrorMessage name="email">
+                  {(err) => <span className="error-message">{err}</span>}
+                </ErrorMessage>
+              </div>
+              <div className="col">
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <Field
+                    id="password"
+                    className="input"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                  />
+                  <ErrorMessage name="password">
+                    {(err) => <span className="error-message">{err}</span>}
+                  </ErrorMessage>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="confirm-password">Confirm Password</label>
+                  <Field
+                    id="confirm-password"
+                    className="input"
+                    name="confirmPassword"
+                    type="password"
+                    placeholder="Confirm Password"
+                  />
+                  <ErrorMessage name="confirmPassword">
+                    {(err) => <span className="error-message">{err}</span>}
+                  </ErrorMessage>
+                </div>
+              </div>
+              <button
+                className="btn btn-add"
+                disabled={!props.isValid || props.isSubmitting}
+              >
+                {props.isSubmitting ? 'Please Wait' : 'Register'}
+              </button>
+              <span className="text-sm">
+                Sudah punya akun? <Link to={'/login'}>Login</Link>
+              </span>
+            </Form>
+          );
+        }}
+      </Formik>
     </>
   );
 };
